8ce4df25d12b37d288aa073d260cb03586dd8149
Friedturtleee <154737973+Friedturtleee@users.noreply.github.com>
2025-12-23 12:00:53 +0800

    新增設定檔和主要程式碼檔案

---
+++ Coding Place/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0);
+#define int long long
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define add(x) accumulate(all(x),0LL);
+using namespace std;
+
+signed main(){
+    fastio
+
+    
+}
